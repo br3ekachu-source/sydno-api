bdc2618f96d053bd3588147a3dddef41e8a2499d
br3ekachu <br3ekachu@gmail.com>
2023-10-21 23:29:19 +0300

    upd

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/home', function () {
-    return 'HOME';
-})->name('home');
 
 // Route::controller(AuthController::class)->group(function(){
 //     Route::post('/auth/register', 'register');
