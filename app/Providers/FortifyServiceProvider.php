a72981695ec4b9645b48659eeb02977a5951ea2f
br3ekachu <br3ekachu@gmail.com>
2023-10-29 02:49:32 +0300

    upd

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -46,11 +49,24 @@ class FortifyServiceProvider extends ServiceProvider
     }
 
     protected function registerResponseBindings() {
+        //переопределяем юрл для восстановления пароля
         ResetPassword::createUrlUsing(function($notifiable, $token) {
             return url(env('SPA_URL'))."/auth/reset-password/{$token}?email={$notifiable->getEmailForPasswordReset()}";
         });
-        VerifyEmail::createUrlUsing(function($notifiable) {              
-            return url(env('SPA_URL'))."/auth/verify/?id={$notifiable->getKey()}&hash=".sha1($notifiable->getEmailForVerification());
+        //переопределяем юрл для подтверждения почты
+        VerifyEmail::createUrlUsing(function($notifiable) {
+            $id = $notifiable->getKey();
+            $hash = sha1($notifiable->getEmailForVerification());
+            $expires = Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60));
+            $url = URL::temporarySignedRoute(
+                'verification.verify',
+                $expires,
+                [
+                    'id' => $id,
+                    'hash' => $hash,
+                ]
+            );
+            return env('SPA_URL').'/auth/verify/?id='.$id.'&hash='.$hash.'&'.parse_url($url)['query'];
         });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdvertController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 
-Route::middleware('auth:sanctum'/*, 'verified'*/)->group(function () {
+Route::middleware('auth:sanctum', 'verified')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::resource('adverts', AdvertController::class);
+    
 });
-
